5ab1a4903cdd0ba69f6f335d7d24de4a565be91f
andres48381 <andres.barroso@simloc.es>
2021-03-09 23:04:11 +0100

    [NEW] Blink standby

--- FirmwareArdu.ino
+++ FirmwareArdu.ino
@@ -1,51 +1,93 @@
 // constants won't change. They're used here to set pin numbers:
 const int buttonPin[]={5,6,7,8};
 const char buttonCode[]={'p','c','b','m'};
-const int ledPin=13;
+const int ledPin[]={25,26,27,28};
+int cont_wait=0;
+bool etapa_wait=true;
 
 // variables will change:
 int buttonState[] = {0,0,0,0};         // variable for reading the pushbutton status
 int buttonState0[] = {0,0,0,0}; 
 
+void waitingSystem()
+{
+  int led=cont_wait/3; //3xCiclos de espera
+
+  if((led>=0) && (led<4))
+  {
+     //Reset
+    clearLed();
+    digitalWrite(ledPin[led],HIGH);
+  }
+  else
+  {
+    cont_wait=0; //Reset
+  }
+
+  cont_wait++;
+
+  //Check serial port
+  if(Serial.available()>0)//Si el Arduino recibe datos a través del puerto serie
+  {
+    byte dato = Serial.read(); //Los almacena en la variable "dato"
+    if(dato==65)  //Si recibe una "A" (en ASCII "65")
+    {
+      etapa_wait=false;
+      cont_wait=0; //Reset
+      digitalWrite(13,HIGH);
+      //Reset
+      clearLed();
+    }
+  }
+  
+}
+void clearLed()
+{
+  for(int i=0;i<4;i++)
+  {
+    digitalWrite(ledPin[i],LOW);
+  }
+}
+
 void setup() {
-  // initialize the LED pin as an output:
+
   for(int i=0;i<4;i++)
   {
     pinMode(buttonPin[i], INPUT);
+    pinMode(ledPin[i], OUTPUT);
   }
-  // initialize the pushbutton pin as an input:
-  pinMode(ledPin, OUTPUT);
 
-    //Puerto de comunicacion en 9600 baudios
+    pinMode(13, OUTPUT);
+  //Etapa espera conexion
+ // etapa_wait=true;
+
+  //Puerto de comunicacion en 9600 baudios
   Serial.begin(9600);
 }
 
 void loop() {
-  // read the state of the pushbutton value:
-  for(int i=0;i<4;i++)
+
+  //ESPERANDO CONEXION
+  if(etapa_wait)
   {
-     buttonState[i] = digitalRead(buttonPin[i]); 
-     
-    if (buttonState[i] == HIGH) {
-
-      if(buttonState0[i]==0){
-        Serial.println(buttonCode[i]);
-      }  
-    }
-       buttonState0[i]=buttonState[i];
+    waitingSystem();  
   }
+  else
+  {  
+    //LECTURA DE PULSADORES
+    for(int i=0;i<4;i++)
+    {
+       buttonState[i] = digitalRead(buttonPin[i]); 
+       
+      if (buttonState[i] == HIGH) {
+  
+        if(buttonState0[i]==0){
+          Serial.println(buttonCode[i]);
+        }  
+      }
+         buttonState0[i]=buttonState[i];
+    }    
+  }    
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
- /* if (buttonState == HIGH) {
-    // turn LED on:
-    digitalWrite(ledPin, HIGH);
-    if(buttonState0==0){
-      Serial.println("p");
-    }
-  } else {
-    // turn LED off:
-    digitalWrite(ledPin, LOW);
-  }
-
-  buttonState0=buttonState;*/
+  delay(150);
 }
