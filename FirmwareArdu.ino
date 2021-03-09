d2760d3efa9ca34843d9af3d39748a0ff99658ba
andres48381 <andres.barroso@simloc.es>
2021-03-09 13:38:03 +0100

    [NEW] Vector juegos para envio

--- FirmwareArdu.ino
+++ FirmwareArdu.ino
@@ -1,16 +1,20 @@
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 5;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+const int buttonPin[]={5,6,7,8};
+const char buttonCode[]={'p','c','b','m'};
+const int ledPin=13;
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-int buttonState0 = 0;
+int buttonState[] = {0,0,0,0};         // variable for reading the pushbutton status
+int buttonState0[] = {0,0,0,0}; 
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  for(int i=0;i<4;i++)
+  {
+    pinMode(buttonPin[i], INPUT);
+  }
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 
     //Puerto de comunicacion en 9600 baudios
   Serial.begin(9600);
@@ -18,10 +22,21 @@ void setup() {
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin);
+  for(int i=0;i<4;i++)
+  {
+     buttonState[i] = digitalRead(buttonPin[i]); 
+     
+    if (buttonState[i] == HIGH) {
+
+      if(buttonState0[i]==0){
+        Serial.println(buttonCode[i]);
+      }  
+    }
+       buttonState0[i]=buttonState[i];
+  }
 
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
+ /* if (buttonState == HIGH) {
     // turn LED on:
     digitalWrite(ledPin, HIGH);
     if(buttonState0==0){
@@ -32,5 +47,5 @@ void loop() {
     digitalWrite(ledPin, LOW);
   }
 
-  buttonState0=buttonState;
+  buttonState0=buttonState;*/
 }
